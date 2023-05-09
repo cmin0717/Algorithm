f70be72472a31be66e5586ff0c0fef2aa18b2959
cmin0717 <cyun0717@gmail.com>
2023-05-10 01:20:47 +0900

    2023.05.09 문제풀이

--- 2023-05(백준,프로그래머스)/택배 배달과 수거하기.py
+++ 2023-05(백준,프로그래머스)/택배 배달과 수거하기.py
@@ -1,27 +1,24 @@
-# def solution(cap, n, deliveries, pickups):
+def solution(cap, n, deliveries, pickups):
     
-#     d,p = {},{}
-    
-#     for i in range(1,n+1):
-#         d.setdefault(i,0)
-#         p.setdefault(i,0)
-    
-#     sd,sp = 0,0
-#     for i in range(n-1,-1,-1):
-#         if deliveries[i]:
-#             sd += deliveries[i]
-#             turn = sd//cap if sd % cap == 0 else (sd//cap) +1
-#             if d[turn] == 0:
-#                 d[turn] = i
-#         if pickups[i]:
-#             sp += pickups[i]
-#             turn = sp//cap if sp % cap == 0 else (sp//cap) +1
-#             if p[turn] == 0:
-#                 p[turn] = i
-#     print(d,p)
+    result = [0] * n
+    sd,sp = 0,0 # 누적합 사용
+     # 각 집을 처리하려면 몇번째 트럭까지와야하는지 담는다.
+    for i in range(n-1,-1,-1):
+        if deliveries[i]:
+            sd += deliveries[i]
+            turn = sd//cap if sd % cap == 0 else (sd//cap) +1
+            result[i] = turn # deliveries을 먼저 체크하기에 여기서는 max를 사용할필요가 없다.
+        if pickups[i]:
+            sp += pickups[i]
+            turn = sp//cap if sp % cap == 0 else (sp//cap) +1
+            result[i] = max(result[i], turn)
+
+    # 그렇게 담은 트럭을 이용하여 최소로 움직일 거리를 구한다.
+    cnt,idx = 0,0
+    for i in range(n-1,-1,-1):
+        t = result[i]
+        if t > idx:
+            cnt += (i+1)*2*(t-idx)
+            idx = t # idx를 이용하여 중복 처리되는것을 방지
         
-
-# solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0])
-
-dic = {}
-print(list(dic.items()))
+    return cnt
